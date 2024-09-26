1b1470db0ea697fad75a0ffde920c3be1c4efdbe
exam <exam@Aptechnk1.edu>
2024-09-26 20:54:39 +0500

    ameen

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -6,14 +6,26 @@ class Welcomescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      color: Colors.black,
-      height: 200,
-      width: 200,
-      child: Text(
-        "Ameen",
-        style: TextStyle(color: Colors.white),
+      body: Container(
+        //margin: EdgeInsets.only(left:),
+        //margin: EdgeInsets.fromLTRB(20,10,0,0),
+        //padding : EdgeInsets.symetric(horizontal:50, vertical:100),
+        // padding: EdgeInsets.all(20),
+        padding: EdgeInsets.only(left: 100, bottom: 50, right: 12),
+        color: const Color.fromARGB(255, 132, 195, 211),
+        height: 200,
+        width: 200,
+        alignment: Alignment.center,
+
+        child: Text(
+          textAlign: TextAlign.center,
+          "Ameen",
+          style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 25,
+              color: const Color.fromARGB(255, 0, 0, 0)),
+        ),
       ),
-    ));
+    );
   }
 }
---
+++ lib/loginScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatelessWidget {
+  const Loginscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "login",
+            style:
+          )
+        ],
+      ),
+    ));
+  }
+}
